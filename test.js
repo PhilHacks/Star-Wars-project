718009cd917d6fca4015b871dbe8753114368e3b
PhilHacks <111358705+PhilHacks@users.noreply.github.com>
2023-05-07 13:21:13 +0200

    Ändrade lite för att lösa problem vid no ch found

--- test.js
+++ test.js
@@ -3,3 +3,24 @@ const axios = require("axios");
 
 let characters = [];
 console.log(characters);
+
+async function addFirstCharacter() {
+  const first = prompt("Add Star Wars character to list! ");
+  await axios
+    .get(`https://swapi.dev/api/people/?search=${first}`)
+    .then((response) => {
+      if (response.data.count === 0) {
+        console.log(`No Star Wars characters found matching ${first}`);
+        addFirstCharacter();
+      } else {
+        response.data.results[0].name;
+        characters.push(first);
+        console.log(characters);
+      }
+    })
+    .catch((error) => {
+      console.log("Error1: Something went wrong, try again ");
+      addFirstCharacter();
+    });
+}
+addFirstCharacter();

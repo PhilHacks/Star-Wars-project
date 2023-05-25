1478fc9795f0784fdf2b8a891be8cb04dff1074e
Filip Nyman <filnym@hotmail.com>
2023-05-25 20:47:55 +0200

    skapade test koppla mongodb

---
+++ test.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const connectionString =
+  "mongodb+srv://filipnyman7:filipnyman7@philscluster0.5wvjvwb.mongodb.net/SWAPI?retryWrites=true&w=majority";
+
+mongoose.connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const characterSchema = new mongoose.Schema({
+  name: String,
+});
+
+const Character = mongoose.model("Character", { name: String });
+
+// ... kod för anslutning och modellskapelse
+
+async function testCode() {
+  try {
+    const newCharacter = new Character({ name: "Luke Skywalker" });
+    await newCharacter.save();
+    console.log("New character saved successfully!");
+  } catch (error) {
+    console.error("Error while saving character:", error);
+  }
+}
+
+// Kalla på testfunktionen
+testCode();

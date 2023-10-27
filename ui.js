3d097ed4e68116126b6caf76f4359bc84e1c2dac
Phil <filip.nyman7@gmail.com>
2023-10-27 14:38:06 +0200

    es6 export functions

--- ui.js
+++ ui.js
@@ -1,49 +1,40 @@
-const prompt = require("prompt-sync")();
+import promptSync from "prompt-sync";
 
-function promptAddCharacter() {
+const prompt = promptSync();
+
+export function promptAddCharacter() {
   return prompt("Add Star Wars character to list: ");
 }
 
-function promptAddAnotherCharacter() {
+export function promptAddAnotherCharacter() {
   return prompt("Add another character to list: ");
 }
 
-function promptRemoveCharacter() {
+export function promptRemoveCharacter() {
   return prompt("Enter index of character to remove it (0, 1 etc.):");
 }
 
-function promptAddSeveralCharacters() {
+export function promptAddSeveralCharacters() {
   const add = prompt("Enter name to add character:");
   const addAnother = prompt("Enter another one:");
   return [add, addAnother];
 }
 
-function promptMoveCharacter() {
+export function promptMoveCharacter() {
   const move = prompt("Enter index of the character you wanna move:");
   const newIndex = prompt("Enter the index where you wanna move it to:");
   return [move, newIndex];
 }
 
-function promptRemoveSeveralCharacters() {
+export function promptRemoveSeveralCharacters() {
   const remove1 = prompt("Enter an index to remove:");
   const remove2 = prompt("Enter another index to remove:");
   return [remove1, remove2];
 }
 
-function printCharacters(characters) {
+export function printCharacters(characters) {
   console.log(characters);
 }
-
-// Exportera alla funktioner
-module.exports = {
-  promptAddCharacter,
-  promptAddAnotherCharacter,
-  promptRemoveCharacter,
-  promptAddSeveralCharacters,
-  promptMoveCharacter,
-  promptRemoveSeveralCharacters,
-  printCharacters,
-};
 
 // Ui.js Förklaring av kod:
 // Koden i ui.js definierar och exporterar olika funktioner för att interagera med användaren och manipulera listan med karaktärer:

41bae6ea7565156c4f39a813722beac2281417e6
Phil <filip.nyman7@gmail.com>
2023-10-27 14:36:04 +0200

    change import to es6 + eng comments

--- app.js
+++ app.js
@@ -1,23 +1,34 @@
 //Huvudfil där allt körs ifrån.
-const { promptAddCharacter } = require("./ui.js");
-const { fetchCharacterData } = require("./swapi.js");
-const { saveCharacterToDatabase } = require("./mongodb.js");
+import {
+  promptAddCharacter,
+  promptAddAnotherCharacter,
+  promptRemoveCharacter,
+  promptAddSeveralCharacters,
+  promptMoveCharacter,
+  promptRemoveSeveralCharacters,
+  printCharacters,
+} from "./ui.js";
+import { fetchCharacterData } from "./swapi.js";
+import { saveCharacterToDatabase } from "./mongodb.js";
 
 async function main() {
   try {
-    // Fråga användaren om vilken Star Wars-karaktär de vill lägga till
+    // Ask the user which Star Wars character they want to add
     const characterName = promptAddCharacter();
 
-    // Hämta karaktärsinformation från SWAPI
+    // Fetch character information from SWAPI
     const characterData = await fetchCharacterData(characterName);
 
-    // Spara karaktärsinformationen i MongoDB
-    await saveCharacterToDatabase(characterData);
-
-    console.log(`Character "${characterName}" has been added to the database!`);
+    // Save character information to MongoDB
+    if (characterData) {
+      await saveCharacterToDatabase(characterData);
+      console.log(
+        `Character "${characterName}" has been added to the database!`
+      );
+    }
   } catch (error) {
     console.error("Error in main application:", error);
   }
 }
 
-main(); // Kör huvudfunktionen
+main();

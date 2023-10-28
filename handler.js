afd507113a6c75af19cfa7f12808e3b76a2eb03e
Phil <filip.nyman7@gmail.com>
2023-10-28 14:41:53 +0200

    add handler.js for seperation of concerns

---
+++ handler.js
@@ -0,0 +1,73 @@
+import {
+  promptAddCharacter,
+  promptRemoveCharacter,
+  promptMoveCharacter,
+  printCharacters,
+  printMessage,
+} from "./ui.js";
+
+import { fetchAndCreateCharacter, fetchMultipleCharacters } from "./swapi.js";
+
+import {
+  saveCharacter,
+  removeCharacter,
+  findCharacterByName,
+  updateMultipleCharacterIndexes,
+  updateCharacterIndexes,
+  sortCharacterIndexes,
+} from "./mongodb.js";
+
+export async function moveCharacterOperation() {
+  const [nameToMove, toNewIndex] = promptMoveCharacter();
+  const characterToMove = await findCharacterByName(nameToMove);
+  if (characterToMove) {
+    const newIndexInt = parseInt(toNewIndex);
+    const query = {
+      index: {
+        $gte: Math.min(characterToMove.index, newIndexInt),
+        $lte: Math.max(characterToMove.index, newIndexInt),
+      },
+    };
+    const update = {
+      $inc: { index: characterToMove.index < newIndexInt ? -1 : 1 },
+    };
+    await updateMultipleCharacterIndexes(query, update);
+    characterToMove.index = newIndexInt;
+    await characterToMove.save();
+    await updateCharacterIndexes();
+    printMessage(
+      `Character "${nameToMove}" moved successfully to index ${toNewIndex}`
+    );
+  } else {
+    printMessage(`Character "${nameToMove}" was not found.`);
+  }
+}
+
+export async function listCharacters() {
+  const sortedCharacters = await sortCharacterIndexes();
+  printCharacters(sortedCharacters);
+}
+
+export async function addCharacter() {
+  const characterName = promptAddCharacter();
+  const characterData = await fetchAndCreateCharacter(characterName);
+  if (characterData) {
+    await saveCharacter(characterData.name);
+    printMessage(
+      `Character "${characterData.name}" has been added to the database!`
+    );
+  } else {
+    printMessage(`Character "${characterName}" was not found.`);
+  }
+}
+
+// Function to handle removing a character
+export async function removeCharacterOperation() {
+  const nameToRemove = promptRemoveCharacter();
+  const removeResult = await removeCharacter(nameToRemove);
+  printMessage(
+    removeResult
+      ? `Character "${nameToRemove}" has been removed.`
+      : `Character "${nameToRemove}" was not found.`
+  );
+}

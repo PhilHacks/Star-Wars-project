d093e5c3488c73dd95516c0a5749763bfb141682
Phil <filip.nyman7@gmail.com>
2023-10-28 14:42:45 +0200

    add func and removed comments

--- swapi.js
+++ swapi.js
@@ -46,12 +46,51 @@ export async function fetchCharacterData(characterName) {
   }
 }
 
-// Swapi.js SFörklaringen av koden:
-// - Axios importeras för att använda för HTTP-anrop.
-// - Funktionen `getCharacterNames` är en async-funktion som hämtar karakternamn från Swapi API.
-// - En while-loop används för att iterera över flera sidor av karaktärer.
-// - Inuti loopen görs ett GET-anrop till nästa sida med Axios.
-// - Resultaten från anropet innehåller en lista med karaktärer och en länk till nästa sida.
-// - Namnen på karaktärerna läggs till i en array genom att loopa över resultaten.
-// - Antalet namn och namnen skrivs ut i konsolen.
-// - Arrayen med namn returneras från funktionen för att användas i andra filer.
+export async function fetchAndCreateCharacter(characterName) {
+  try {
+    const response = await axios.get(
+      `https://swapi.dev/api/people/?search=${characterName}`
+    );
+
+    if (response.data.results.length === 0) {
+      console.log(`No Star Wars characters found matching "${characterName}"`);
+      return null;
+    } else {
+      return {
+        name: response.data.results[0].name,
+      };
+    }
+  } catch (error) {
+    console.error("Error: Something went wrong, try again", error);
+    return null;
+  }
+}
+
+export async function fetchMultipleCharacters(namesArray) {
+  const charactersData = [];
+
+  for (let i = 0; i < namesArray.length; i++) {
+    const name = namesArray[i];
+
+    try {
+      const response = await axios.get(
+        `https://swapi.dev/api/people/?search=${name}`
+      );
+
+      if (response.data.results.length === 0) {
+        console.log(`No Star Wars characters found matching "${name}"`);
+        continue;
+      }
+
+      const characterData = {
+        name: response.data.results[0].name,
+      };
+
+      charactersData.push(characterData);
+    } catch (error) {
+      console.error(`Error fetching character "${name}":`, error);
+    }
+  }
+
+  return charactersData;
+}

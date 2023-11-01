16d1e9e1ceba00112869e0d9fed0416dc36e401b
Phil <filip.nyman7@gmail.com>
2023-11-01 12:50:17 +0100

    change func to arrow func

--- ui.js
+++ ui.js
@@ -2,44 +2,44 @@ import promptSync from "prompt-sync";
 
 const prompt = promptSync();
 
-export function promptAddCharacter() {
+export const promptAddCharacter = () => {
   return prompt("Add Star Wars character to list: ");
-}
+};
 
-export function promptRemoveCharacter() {
+export const promptRemoveCharacter = () => {
   return prompt("Enter the name of the character to delete: ");
-}
+};
 
-export function promptMoveCharacter() {
+export const promptMoveCharacter = () => {
   const name = prompt("Enter the name of the character to move: ");
   const toNewIndex = prompt("Enter the new index for the character: ");
   return [name, toNewIndex];
-}
+};
 
-export function promptAddMultipleCharacters(count) {
+export const promptAddMultipleCharacters = (count) => {
   const namesArray = [];
   for (let i = 0; i < count; i++) {
     const name = prompt(`Enter the name of character ${i + 1}: `);
     namesArray.push(name);
   }
   return namesArray;
-}
+};
 
-export function promptRemoveMultipleCharacters(count) {
+export const promptRemoveMultipleCharacters = (count) => {
   const namesArray = [];
   for (let i = 0; i < count; i++) {
     const name = prompt(`Enter the name of character ${i + 1} to remove: `);
     namesArray.push(name);
   }
   return namesArray;
-}
+};
 
-export function printCharacters(characters) {
+export const printCharacters = (characters) => {
   characters.forEach((character) => {
     console.log(`Index: ${character.index} Name: ${character.name} `);
   });
-}
+};
 
-export function printMessage(message) {
+export const printMessage = (message) => {
   console.log(message);
-}
+};

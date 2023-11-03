a6de3a79257fee7a2c1380d5c6e566639bf69f25
Phil <filip.nyman7@gmail.com>
2023-11-03 11:14:20 +0100

    cahnge import to mongoOperations.js

--- handler.js
+++ handler.js
@@ -20,7 +20,7 @@ import {
   moveCharacterToNewIndex,
   updateCharacterIndexes,
   sortCharacterIndexes,
-} from "./mongodb.js";
+} from "./mongoOperations.js";
 
 export const listStarWarsCharacters = async () => {
   try {

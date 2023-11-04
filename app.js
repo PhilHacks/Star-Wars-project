46a4bef85a057a4e27c6bd746b33ded5cba263ce
Phil <filip.nyman7@gmail.com>
2023-11-04 14:22:37 +0100

    made user iterface easier by just using 1 letter

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import promptSync from "prompt-sync";
 const prompt = promptSync();
 
 import { connectToMongoDb, closeConnectionToMongoDb } from "./mongoConnection.js";
+import { welcomeMessage } from "./ui.js";
 import {
   addStarWarsCharacter,
   removeStarWarsCharacter,
@@ -13,27 +14,30 @@ import {
 
 // Object mapping user input strings to the corresponding functions
 const userCommandObj = {
-  'add': addStarWarsCharacter,
-  'remove': removeStarWarsCharacter,
-  'move': moveStarWarsCharacter,
-  'list': listStarWarsCharacters,
-  'add many': addMultipleCharacters,
-  'remove many': removeMultipleCharacters,
+  'a': addStarWarsCharacter,
+  'r': removeStarWarsCharacter,
+  'm': moveStarWarsCharacter,
+  'l': listStarWarsCharacters,
+  'am': addMultipleCharacters,
+  'rm': removeMultipleCharacters,
 };
 
 // Main function to execute user commands until 'exit' is entered
 const runApp = async () => {
+  
   try {
+    welcomeMessage();
+    
     await connectToMongoDb();
     await listStarWarsCharacters();
     let userCommand;
-    do {userCommand = prompt("Enter the operation you want to do (add, remove, move, list, add many, remove many, exit): ").toLowerCase();
+    do {userCommand = prompt("🎮 Do you wish to: Add (A), Remove (R), Move (M), See List (L), Add Many (Am), Remove Many (Rm), or exit (e)?: ").toLowerCase();
       if (userCommandObj[userCommand]) {
         await userCommandObj[userCommand]();
-      } else if (userCommand !== "exit") {
+      } else if (userCommand !== "e") {
         console.log("Invalid operation. Try again.");
       }
-    } while (userCommand !== "exit");
+    } while (userCommand !== "e");
     await closeConnectionToMongoDb();
   } catch (error) {
     console.error("Error in main application:", error);

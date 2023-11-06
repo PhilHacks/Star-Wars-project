72f56eb2bf26adc1330fe55856640cf61c184ec6
Phil <filip.nyman7@gmail.com>
2023-11-06 21:29:07 +0100

    add basic routes to server

--- server.js
+++ server.js
@@ -1,18 +1,43 @@
+import { listStarWarsCharacters, addStarWarsCharacter, removeStarWarsCharactersByIndex} from "./handler.js"
+
 import { express } from "express";
 
-const app = express();
 
+const app = express();
 app.use(express.json());
 
+// Route list all characters
+app.get('/characters', async (req, res) =>{
+    try {
+        const characters = await listStarWarsCharacters();
+        res.json(characters);
+      } catch (error) {
+        res.status(500).send(error.message);
+      }
+})
 
-// Route to list all characters
-app.get()
+// Route add new character
+app.post('/characters', async (req, res) => {
+    try {
+        const { name } = req.body;
+        await addStarWarsCharacter(name);
+        res.status(201).send(`Character ${name} added successfully.`);
+      } catch (error) {
+        res.status(500).send(error.message);
+      }
+})
 
-// Route to add a new character
-app.post()
 
-// Route to remove a character by index
-app.delete()
+// Route remove character by index
+app.delete('/characters:index', async (req, res) => {
+    try {
+        const { index } = req.params;
+        await removeStarWarsCharacterByIndex(index);
+        res.send(`Character at index ${index} removed successfully.`);
+      } catch (error) {
+        res.status(500).send(error.message);
+      }
+})
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {

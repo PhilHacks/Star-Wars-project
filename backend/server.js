21650f2550ecd534174ef528050dea2c72e25b18
Phil <filip.nyman7@gmail.com>
2024-02-20 16:37:38 +0100

    change port to not cause problem with port for react.js

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,27 @@
 import { listStarWarsCharacters, addStarWarsCharacter, removeStarWarsCharactersByIndex} from "./handler.js"
-
+import {sortCharacterIndexes } from "./mongoOperations.js"
+// import cors from "cors"
 import express  from "express";
 
 
 const app = express();
 app.use(express.json());
+//middleware for axios for React
+// app.use(cors());
 
 // Route list all characters
 app.get('/characters', async (req, res) =>{
     try {
         const characters = await listStarWarsCharacters();
-        res.json(characters);
+        res.status(200).json(characters);
+        console.log(characters)
       } catch (error) {
         res.status(500).send(error.message);
       }
 })
 
 // Route add new character
-app.post('/characters/add', async (req, res) => {
+app.post('/characters/add', async (req, res) => { 
     try {
         const { name } = req.body;
         await addStarWarsCharacter(name);
@@ -40,7 +44,7 @@ app.delete('/characters/remove/:index', async (req, res) => {
 })
 
 export const startServer = async () => {
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });

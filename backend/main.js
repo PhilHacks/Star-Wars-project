eb412d903b5027e87de2393f4589913e46f00067
Phil <filip.nyman7@gmail.com>
2024-02-23 08:13:49 +0100

    remove terminal based code

--- backend/main.js
+++ backend/main.js
@@ -1,44 +1,12 @@
-import promptSync from "prompt-sync";
-const prompt = promptSync();
-
 import { connectToMongoDb, closeConnectionToMongoDb } from "./mongoConnection.js";
-import { welcomeMessage } from "./ui.js";
-import {
-  addStarWarsCharacter,
-  removeStarWarsCharactersByIndex,
-  moveStarWarsCharacter,
-  listStarWarsCharacters,
-} from "./handler.js";
-
 import { startServer } from "./server.js"
 
-// Object mapping user input strings to the corresponding functions
-const userCommandObj = {
-  'a': addStarWarsCharacter,
-  'r': removeStarWarsCharactersByIndex,
-  'm': moveStarWarsCharacter,
-  'l': listStarWarsCharacters,
-};
 
-// Main function to execute user commands until 'exit' is entered
 const runApp = async () => {
   
-  try {
-    welcomeMessage();
-    
+  try {    
     await connectToMongoDb();
     await startServer();
-    await listStarWarsCharacters();
-    // let userCommand;
-    // do {userCommand = 
-    //   prompt("🎮 Do you wish to: Add (a), Remove (r), Move (m), See List (l), or exit (e)?: ").toLowerCase();
-    //   if (userCommandObj[userCommand]) {
-    //     await userCommandObj[userCommand]();
-    //   } else if (userCommand !== "e") {
-    //     console.log("Invalid operation. Try again.");
-    //   }
-    // } while (userCommand !== "e");
-    // await closeConnectionToMongoDb();
   } catch (error) {
     console.error("Error in main application:", error);
     await closeConnectionToMongoDb();

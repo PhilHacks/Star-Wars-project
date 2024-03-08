3a6c4a2f2e4faf3ca6d095881b7242828b500e1f
Phil <filip.nyman7@gmail.com>
2024-03-08 10:44:19 +0100

    add try catch to addCHaracter func

--- swapi-frontend/src/services/CharacterService.js
+++ swapi-frontend/src/services/CharacterService.js
@@ -4,8 +4,15 @@ const baseUrl = "http://localhost:5000/characters";
 
 export const fetchCharacters = () => axios.get(baseUrl);
 
-export const addCharacter = (characterName) =>
-  axios.post(`${baseUrl}/add`, { characterName });
+export const addCharacter = async (characterName) => {
+  try {
+    const response = await axios.post(`${baseUrl}/add`, { characterName });
+    return response;
+  } catch (error) {
+    console.error("Errror fetching characters", error);
+    throw error;
+  }
+};
 
 export const removeCharacter = (id) => axios.delete(`${baseUrl}/remove/${id}`);
 

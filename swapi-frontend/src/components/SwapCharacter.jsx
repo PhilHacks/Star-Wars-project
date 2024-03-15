1955995303a49d1cb394331c54c74d8136ff16a0
Phil <filip.nyman7@gmail.com>
2024-03-15 13:26:55 +0100

    css in js styles and removal of console.log

--- swapi-frontend/src/components/SwapCharacter.jsx
+++ swapi-frontend/src/components/SwapCharacter.jsx
@@ -1,31 +1,64 @@
 import { useState } from "react";
 import { swapCharacters } from "../services/CharacterService";
+import SpinnerComponent from "./SpinnerComponent";
+import MessageComponent from "./MessageComponent";
+import styled from "styled-components";
 
+const SwapButton = styled.button`
+  padding: 5px;
+  cursor: pointer;
+`;
 
-function SwapCharacter({ fetchCharactersAndUpdate, characters} ) {
+const StyledSelect = styled.select`
+  padding: 5px;
+  margin: 0;
+`;
+
+function SwapCharacter({ fetchCharactersAndUpdate, characters }) {
   const [selectedCharacter1, setSelectedCharacter1] = useState("");
   const [selectedCharacter2, setSelectedCharacter2] = useState("");
-  
+  const [loading, setLoading] = useState(false);
+  const [message, setMessage] = useState("");
+
   const swapSelectedCharacters = () => {
-    console.log("Selected characters:", selectedCharacter1, selectedCharacter2);
+    setLoading(true);
+
     if (!selectedCharacter1 || !selectedCharacter2) {
-      console.log("Plese select two characters to swap");
+      setMessage("Please select two characters to swap!");
+      setLoading(false);
+      setTimeout(() => setMessage(""), 3000);
       return;
     }
+
+    if (selectedCharacter1 === selectedCharacter2) {
+      setMessage("Choose two different characters to swap!");
+      setSelectedCharacter1("");
+      setSelectedCharacter2("");
+      setLoading(false);
+      setTimeout(() => setMessage(""), 3000);
+      return;
+    }
+
     swapCharacters(selectedCharacter1, selectedCharacter2)
       .then(() => {
         fetchCharactersAndUpdate();
         setSelectedCharacter1("");
         setSelectedCharacter2("");
+        setMessage("Characters Swapped!");
+        setLoading(false);
+        setTimeout(() => setMessage(""), 3000);
       })
-      .catch((err) => console.log(err));
+      .catch((err) => {
+        console.log(err);
+        setMessage("Failed to Swap");
+        setLoading(false);
+        setTimeout(() => setMessage(""), 3000);
+      });
   };
-  
-  
-  
+
   return (
-    <>
-      <select
+    <div>
+      <StyledSelect
         value={selectedCharacter1}
         onChange={(e) => setSelectedCharacter1(e.target.value)}
       >
@@ -35,8 +68,8 @@ function SwapCharacter({ fetchCharactersAndUpdate, characters} ) {
             {character.name}
           </option>
         ))}
-      </select>
-      <select
+      </StyledSelect>
+      <StyledSelect
         value={selectedCharacter2}
         onChange={(e) => setSelectedCharacter2(e.target.value)}
       >
@@ -45,11 +78,13 @@ function SwapCharacter({ fetchCharactersAndUpdate, characters} ) {
           <option key={character._id} value={character._id}>
             {character.name}
           </option>
-            ))}
-      </select>
-      <button onClick={swapSelectedCharacters}>Swap Characters</button>
-    </>
-     )
+        ))}
+      </StyledSelect>
+      <SwapButton onClick={swapSelectedCharacters}>Swap</SwapButton>
+      {loading ? <SpinnerComponent /> : null}
+      {message && <MessageComponent message={message} />}
+    </div>
+  );
 }
 
-export default SwapCharacter
+export default SwapCharacter;
